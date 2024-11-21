a5edda14a2a41ef8f937078b45338b8c246ad893
danepaixao <selettorestaurante@gmail.com>
2024-11-21 18:22:02 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,40 +1,30 @@
+
 import pandas as pd
 import plotly.express as px
 import streamlit as st
 
-# Leitura dos dados
-car_data = pd.read_csv('vehicles_us.csv')
+car_data = pd.read_csv('vehicles_us.csv') # lendo os dados
 
-# Cabeçalho do aplicativo
-st.header('Análise Exploratória dos Dados de Anúncios de Carros')
+st.header('Dashboard de Veículos - Webinar Sprint 5')
 
-# Botão para criar o histograma
-hist_button = st.button('Criar histograma')
+hist_button = st.button('Criar histograma') # criar um botão
+if hist_button: # se o botão for clicado
+    # escrever uma mensagem
+    st.write('Criando um histograma para o conjunto de dados de anúncios de vendas de carros')
 
-if hist_button:
-    st.write('Criando um histograma para a coluna odometer')
-    fig = px.histogram(car_data, x="odometer")  # Criando o histograma
-    st.plotly_chart(fig, use_container_width=True)  # Exibindo o gráfico
-
-# Botão para criar o gráfico de dispersão
-scatter_button = st.button('Criar gráfico de dispersão')
-
-if scatter_button:
-    st.write('Criando um gráfico de dispersão entre odometer e price')
-    fig2 = px.scatter(car_data, x="odometer", y="price")  # Criando o gráfico de dispersão
-    st.plotly_chart(fig2, use_container_width=True)  # Exibindo o gráfico
-
-# (Opcional) Usando caixas de seleção em vez de botões
-build_histogram = st.checkbox('Criar um histograma')
-
-if build_histogram:
-    st.write('Criando um histograma para a coluna odometer')
+    # criar um histograma
     fig = px.histogram(car_data, x="odometer")
+
+    # exibir um gráfico Plotly interativo
     st.plotly_chart(fig, use_container_width=True)
 
-build_scatter = st.checkbox('Criar um gráfico de dispersão')
+disp_button = st.button('Criar gráfico de dispersão') # criar um botão
+if disp_button: # se o botão for clicado
+    # escrever uma mensagem
+    st.write('Criando um gráfico de dispersão para o conjunto de dados de anúncios de vendas de carros')
 
-if build_scatter:
-    st.write('Criando um gráfico de dispersão entre odometer e price')
-    fig2 = px.scatter(car_data, x="odometer", y="price")
-    st.plotly_chart(fig2, use_container_width=True)
+    # criar um histograma
+    fig = px.scatter(car_data, x="odometer", y="price")
+
+    # exibir um gráfico Plotly interativo
+    st.plotly_chart(fig, use_container_width=True)
